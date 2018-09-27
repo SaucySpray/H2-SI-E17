3bf13056e71cb1ecebe7e12c3bf7089d7fe03243
Alexandre <alexandrechabeau.pro@gmail.com>
2018-09-27 13:28:13 +0200

    3D models add

---
+++ js/model.js
@@ -0,0 +1,53 @@
+var canvas = document.getElementById("renderCanvas");
+
+var delayCreateScene = function () {
+  // Create a scene.
+  var scene = new BABYLON.Scene(engine);
+
+  // Create a default skybox with an environment.
+  var hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("textures/environment.dds", scene);
+  var currentSkybox = scene.createDefaultSkybox(hdrTexture, true);
+
+  // Append glTF model to scene.
+  BABYLON.SceneLoader.Append("medias/models/", "light.glb", scene, function (scene) {
+    // Create a default arc rotate camera and light.
+    scene.createDefaultCameraOrLight(true, true, true);
+    // The default camera looks at the back of the asset.
+    // Rotate the camera by 180 degrees to the front of the asset.
+    scene.activeCamera.alpha += Math.PI;
+  });
+  BABYLON.SceneLoader.Append("medias/models/", "lighter.glb", scene, function (scene) {
+    // Create a default arc rotate camera and light.
+    scene.createDefaultCameraOrLight(true, true, true);
+    // The default camera looks at the back of the asset.
+    // Rotate the camera by 180 degrees to the front of the asset.
+    scene.activeCamera.alpha += Math.PI;
+  });
+  BABYLON.SceneLoader.Append("medias/models/", "plate.glb", scene, function (scene) {
+    // Create a default arc rotate camera and light.
+    scene.createDefaultCameraOrLight(true, true, true);
+    // The default camera looks at the back of the asset.
+    // Rotate the camera by 180 degrees to the front of the asset.
+    scene.activeCamera.alpha += Math.PI;
+  });
+  BABYLON.SceneLoader.Append("medias/models/", "speaker.glb", scene, function (scene) {
+    // Create a default arc rotate camera and light.
+    scene.createDefaultCameraOrLight(true, true, true);
+    // The default camera looks at the back of the asset.
+    // Rotate the camera by 180 degrees to the front of the asset.
+    scene.activeCamera.alpha += Math.PI;
+  });
+}
+var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
+var scene = delayCreateScene();
+
+engine.runRenderLoop(function () {
+  if (scene) {
+    scene.render();
+  }
+});
+
+// Resize
+window.addEventListener("resize", function () {
+  engine.resize();
+});
