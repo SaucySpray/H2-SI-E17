12cbdc50dfe2d7d173404968479241a204731f20
grossyoan <32864409+grossyoan@users.noreply.github.com>
2018-09-27 14:36:55 +0200

    Camera fixed

--- js/model.js
+++ js/model.js
@@ -1,40 +1,37 @@
 var canvas = document.getElementById("renderCanvas");
-
+var light, lighter, plate, speaker;
 var delayCreateScene = function () {
   // Create a scene.
   var scene = new BABYLON.Scene(engine);
-  var camera = new BABYLON.FreeCamera("sceneCamera", new BABYLON.Vector3(0, 1, -15), scene);
-camera.inputs.add(new BABYLON.FreeCameraGamepadInput());
-camera.inputs.attached.gamepad.gamepadAngularSensibility = 250;
-
+  var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2.5, -Math.PI / 3.5, 25, BABYLON.Vector3.Zero(), scene);
   // Create a default skybox with an environment.
   scene.clearColor = new BABYLON.Color4(0, 0, 0, 0.0000000000000001);
 
   // Append glTF model to scene.
-  BABYLON.SceneLoader.Append("medias/models/", "light.glb", scene, function (scene) {
+  light = BABYLON.SceneLoader.Append("medias/models/", "light.glb", scene, function (scene) {
     // Create a default arc rotate camera and light.
-    scene.createDefaultCameraOrLight(true, true, true);
+    scene.createDefaultCameraOrLight(true, false, false);
     // The default camera looks at the back of the asset.
     // Rotate the camera by 180 degrees to the front of the asset.
     scene.activeCamera.alpha += Math.PI;
   });
-  BABYLON.SceneLoader.Append("medias/models/", "lighter.glb", scene, function (scene) {
+  lighter = BABYLON.SceneLoader.Append("medias/models/", "lighter.glb", scene, function (scene) {
     // Create a default arc rotate camera and light.
-    scene.createDefaultCameraOrLight(true, true, true);
+    scene.createDefaultCameraOrLight(true, false, false);
     // The default camera looks at the back of the asset.
     // Rotate the camera by 180 degrees to the front of the asset.
     scene.activeCamera.alpha += Math.PI;
   });
-  BABYLON.SceneLoader.Append("medias/models/", "plate.glb", scene, function (scene) {
+  plate = BABYLON.SceneLoader.Append("medias/models/", "plate.glb", scene, function (scene) {
     // Create a default arc rotate camera and light.
-    scene.createDefaultCameraOrLight(true, true, true);
+    scene.createDefaultCameraOrLight(true, false, false);
     // The default camera looks at the back of the asset.
     // Rotate the camera by 180 degrees to the front of the asset.
     scene.activeCamera.alpha += Math.PI;
   });
-  BABYLON.SceneLoader.Append("medias/models/", "speaker.glb", scene, function (scene) {
+  speaker = BABYLON.SceneLoader.Append("medias/models/", "speaker.glb", scene, function (scene) {
     // Create a default arc rotate camera and light.
-    scene.createDefaultCameraOrLight(true, true, true);
+    scene.createDefaultCameraOrLight(true, false, false);
     // The default camera looks at the back of the asset.
     // Rotate the camera by 180 degrees to the front of the asset.
     scene.activeCamera.alpha += Math.PI;
